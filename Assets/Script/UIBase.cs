bfb752a679cc9f8dc3c987b04bb52737a6d54c93
KoheiNagura <k.nagura.p@gmail.com>
2019-07-01 16:20:02 +0900

    add : タイトル画面のボタンが使えるように

---
+++ Assets/Script/UIBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIBase : MonoBehaviour{
+    public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ChangeActive(GameObject obj){
+        obj.SetActive(!obj.activeSelf);
+    }
+}
