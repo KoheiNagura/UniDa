7c4fdd8a18a8cc13d83afea0df6b356110315515
KoheiNagura <k.nagura.p@gmail.com>
2019-07-20 14:23:03 +0900

    update : UIの値が反映されるように

--- Assets/Script/ResultUI.cs
+++ Assets/Script/ResultUI.cs
@@ -8,6 +8,9 @@ public class ResultUI : UIBase{
     [SerializeField] private RectTransform title, score, other;
     [SerializeField] private Text scoreText, correctText, missText, rateText;
     void Start(){
+        correctText.text = Main.correctCount.ToString("D3");
+        missText.text = Main.missCount.ToString("D3");
+        rateText.text = Mathf.RoundToInt(((float)Main.correctCount / (Main.correctCount + Main.missCount)) * 100).ToString("D3");
         float displayScore = 0;
         DOTween.Sequence()
             .AppendInterval(0.3f)
@@ -18,17 +21,19 @@ public class ResultUI : UIBase{
                 () => displayScore,
                 i => {
                     displayScore = i;
-                    scoreText.text = displayScore.ToString("D6");
+                    scoreText.text = Mathf.CeilToInt(displayScore).ToString("D6");
                 },
-                10000,
-                1.5f
+                Main.score,
+                1.3f
             ))
+            .AppendInterval(0.2f)
             .Append(score.DOAnchorPos(Vector2.zero, 0.3f))
             .Join(other.DOAnchorPos(Vector2.zero, 0.5f))
             .OnComplete(() => {
                 title.anchoredPosition = new Vector2(0, 180);
                 score.anchoredPosition = Vector2.zero;
                 other.anchoredPosition = Vector2.zero;
+
             }).Play();
     }
     void Update(){
