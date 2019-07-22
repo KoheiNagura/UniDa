ffe4a1de33f532a13630f077d112a0a9eb3ee832
KoheiNagura <k.nagura.p@gmail.com>
2019-07-22 09:10:32 +0900

    update : jsonの読み込みをResourcesからおこなうように

--- Assets/Script/Manager.cs
+++ Assets/Script/Manager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 using System.Linq;
 
 public class Manager : SingletonMonoBehaviour<Manager>{
@@ -14,8 +13,9 @@ public class Manager : SingletonMonoBehaviour<Manager>{
     void Awake(){
         DontDestroyOnLoad(this.gameObject);
         //json読み込み
-        string filePath = Application.dataPath + "/Data/data.json";
-        string json = File.ReadAllText(filePath);
+        TextAsset txt = Instantiate(Resources.Load("data")) as TextAsset;
+        string json = txt.text;
+        print(json);
         data = JsonUtility.FromJson<TypingData>(json);
         data.words = data.words.OrderBy((content) => content.Length).ToArray();
     }
