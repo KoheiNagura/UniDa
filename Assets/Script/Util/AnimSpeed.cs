6d169f971302efe50608142a3967619d09103a02
KoheiNagura <k.nagura.p@gmail.com>
2020-05-26 10:39:03 +0900

    remove : フォルダ分け

--- Assets/Script/Util/AnimSpeed.cs
+++ Assets/Script/Util/AnimSpeed.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class AnimSpeed : MonoBehaviour{
     private Animator anim;
     [SerializeField, Range(0,5)] private float animSpeed;
-    void Start(){
+
+    private void Start() {
         anim = GetComponent<Animator>();
         SetSpeed(animSpeed);
     }
-    public void SetSpeed(float speed){
-        anim.speed = speed;
-    }
+
+    public void SetSpeed(float speed) => anim.speed = speed;
 }
