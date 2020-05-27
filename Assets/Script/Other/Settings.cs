b26eb7cc8d7f566b2cc65ae542f22f4659365f23
KoheiNagura <k.nagura.p@gmail.com>
2020-05-27 09:43:58 +0900

    update Settings

--- Assets/Script/Other/Settings.cs
+++ Assets/Script/Other/Settings.cs
@@ -9,10 +9,22 @@ public class Settings {
         _playSE,
         _playTypeSound,
         _playMissSound;
-    public static bool PlayBGM => _i._playBGM;
-    public static bool PlaySE => _i._playSE;
-    public static bool PlayTypeSound => _i._playTypeSound;
-    public static bool PlayMissSound => _i._playMissSound;
+    public static bool PlayBGM {
+        get { return _i._playBGM; }
+        set { _i._playBGM = value; }
+    }
+    public static bool PlaySE {
+        get { return _i._playSE; }
+        set { _i._playSE = value; }
+    }
+    public static bool PlayTypeSound {
+        get { return _i._playTypeSound; }
+        set { _i._playTypeSound = value; }
+    }
+    public static bool PlayMissSound {
+        get { return _i._playMissSound; }
+        set { _i._playMissSound = value; }
+    }
 
     public static void Initialize() => Load();
 
@@ -25,6 +37,12 @@ public class Settings {
         var key = Const.SaveKeys.Settings;
         if(PlayerPrefs.HasKey(key)) {
             _i = JsonUtility.FromJson<Settings>( PlayerPrefs.GetString(key) );
-        }else _i = new Settings();
+        }else{
+            _i = new Settings();
+            PlayBGM = true;
+            PlaySE = true;
+            PlayTypeSound = true;
+            PlayMissSound = true;
+        }
     }
 }
